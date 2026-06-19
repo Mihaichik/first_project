4160914e0f0646533dcf162ca391ff0b296428b8
Elena Mikheeva <elena-m072007@yandex.ru>
2026-06-19 15:44:33 +0500

    Add dotenv library and print_author function

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('Hellj from repository!')
+import os
+from dotenv import load_dotenv
+
+# Загружаем переменные из файла .env в систему
+load_dotenv()
+
+def print_author():
+    # Читаем переменную AUTHOR из окружения
+    author = os.getenv("AUTHOR")
+    print(f"Автор проекта: {author}")
+
+# Этот блок запустит функцию при старте файла
+if __name__ == "__main__":
+    print_author()
